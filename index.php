3f650d68e77c83c1005d5310493b30219324d4b7
Cristian samir quejada cordoba <cristiansamir2001@hotmail.com>
2020-11-01 18:01:28 +0100

    se agrega tipografia de terminator

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head> 
+        <title>
+        TALLER 1 PHP
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+</head>
+<body>
+<!-- esto es un titulo!-->
+
+                    <h1>Biografia de Juan guillermo cuadrado </h1>
+
+
+
+<!--biografia!-->
+
+<h4>  Juan Guillermo nació en Necoclí, un pueblo del departamento de Antioquia azotado por la violencia en los años 90. Cuando tenía sólo cuatro años perdió a su padre producto de la violencia.
+
+Unos años después, su mamá, Marcela Bello, lo matriculó en una escuela de fútbol, y para poder pagar la mensualidad, se fue a trabajar a Apartadó, dejando al pequeño con sus tíos. Los fines de semana volvía a Necoclí para estar con su hijo y terminar el bachillerato por las noches.
+
+Se inició en el Club Atlético Urabá y luego hizo parte del Rionegro de la Primera B de Colombia, y luego de su buen desempeño ficho con el Deportivo Independiente Medellín a los 20 años. En este equipo de la Primera A de Colombia debutó el 14 de septiembre de 2009 frente al Boyacá Chicó partido en el que anotó su primer gol como profesional.
+
+</h4>
+
+<h3>Cuadrado llega a Europa</h3>
+
+<h4>En ese mismo año, el 2 de julio y con 21 años, Cuadrado fichó con el Udinense, un club italiano de la primera
+ división, con un contrato por cinco años. Su primer partido fue en la Liga Italiana contra el Chievo Verona como 
+ titular, sin marcar goles. Un año después, fue cedido al Lecce, club que para el momento debutaba en la A. 
+ Cuadrado debutó en la temporada 2011-12 frente al AC Cesena, en el que anotó cuatro goles convirtiéndose 
+ en figura. Anotó 11 goles en 33 partidos en su paso por el club italiano, pese a su actuación,
+ el club descendió a la B.</h4>
+
+ <h3>Cuadrado en el ACF Fiorentina</h3>
+
+ <h4> En julio de 2012, fue cedido al club Fiorentina y es aquí donde se consagra como futbolista con 
+ 20 goles en 60 partidos para la temporada 2012-13, y la clasificación del club a la UEFA Europe League.
+  Para la temporada 2013-14 Cuadrado anotó 22 goles en 32 partidos.
+   Dieciocho goles en serie A, tres en Europe League y uno en Copa Italia.</h4>
+
+   <h3>Llegada al fútbol inglés</h3>
+
+   <h4>El 2 de febrero de 2015 fue fichado por el FC Chelsea, firmando un contrato por 35 millones de euros
+    y 4 años. Debutó como titular el 11 de febrero frente a Everton, donde su equipo ganó 1-0.
+</h4>
+
+<h3>De vuelta al fútbol italiano</h3>
+
+<h4>El 23 de agosto de ese mismo año Cuadrado fue cedido a la Juventus y debutó por primera vez el 30 de agosto
+ entrando en el minuto 75. Debutó como titular en la Champions League el 15 de septiembre en la victoria sobre
+  el Manchester City, teniendo una actuación que llamó la atención del público en general y la prensa.
+   Luego de esto el Chelsea y la Juventus llegaron a un acuerdo por el jugador de 22 millones de euros,
+    pasando de forma definitiva al club italiano. Su primera anotación la hizo el 30 de octubre frente al Torino,
+     con una destacada actuación que continuará con 15 triunfos consecutivos que llevó al
+      equipo italiano al primer puesto de la tabla, terminando campeón del Scudetto y de la Copa de Italia.</h4>
+
+<h3>Cuadrado llega a la Selección Absoluta</h3>
+
+<h4>Llegó con su equipo a cuartos de final de la Copa América, pero no figuró como titular en ningún partido y
+ sólo reemplazó a Freddy Guarín en el minuto 50 del partido frente a Bolivia.
+
+La primera vez que fue convocado fue para una serie de amistosos contra Venezuela en 2010.
+ Pero fue en 2014 que el técnico José Pekerman lo incluyó en la lista inicial de 30 jugadores que irían al
+  mundial para luego ser seleccionado en la nómina definitiva. Sus asistencias en el mundial se convirtieron
+   en goles memorables, y además llevaron a Colombia a cuartos de final.
+
+El 11 de mayo de 2015 y después de un excelente desempeño en la copa del mundo, fue seleccionado por
+ Pekerman como parte de la nómina para la Copa América en Chile.</h4>
+
+ <h3>Eliminatorias a la Copa del Mundo Rusia 2018</h3>
+
+ <h4>En las eliminatorias 2016-17 a la Copa del Mundo, Cuadrado fue pilar fundamental, siendo titular de 12 fechas
+  siendo siempre una de las figuras más importantes dentro de la cancha con proyecciones, velocidad y 
+  desequilibrio por la banda derecha, apoyando además a Santiago Arias. Anotó un gol frente a Ecuador y
+   realizó tres asistencias, en los encuentros frente a Perú, Ecuador y Uruguay.
+
+Juan Guillermo Cuadrado ha hecho 44 goles a lo largo de su carrera, siete de ellos con la camiseta de la
+ Selección Colombia. Tiene desde hace unos años la nacionalidad italiana, y está casado con la colombiana 
+ Melissa Botero, con quien tiene una niña.<h4>
+
+
+<!--para agregar una imagen!-->
+
+<img id="cuadradito" src="images/cuadradito.jpg" width="400" height="250">  <img id="cuadrado" src="images/florentina.jpg" width="400" height="250">
+<img id="chelsea" src="images/chelsea.jpg" width="400" height="250"> <img id="juventus" src="images/juventus.jpg" width="400" height="250">
+<img id="colombia" src="images/colombia.jpg" width="400" height="250"> <img id="mundial" src="images/mundial.jpg" width="400" height="250">
+
+
+</body>
+
+
+</html>
